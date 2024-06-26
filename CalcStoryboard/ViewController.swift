9555ecf5b4a99a1116da3fa8f054a5613b163357
soobeen27 <youdew27@gmail.com>
2024-06-26 20:54:14 +0900

    calculator app with storyboard

--- CalcStoryboard/ViewController.swift
+++ CalcStoryboard/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var expression = "0" {
+        didSet {
+            Buttons.operators.forEach {
+                if expression == $0 {
+                    expression = "0"
+                }
+            }
+            numLabel.text = expression
+        }
+    }
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setButtons()
     }
+    func setButtons() {
+        for index in 0..<buttons.count {
+            buttons[index].setTitle(Buttons.texts[index], for: .normal)
+            buttons[index].layer.cornerRadius = buttons[index].frame.height / 2
+        }
 
-
+        buttons.forEach {
+            $0.addTarget(self, action: #selector(btnsTapped), for: .touchDown)
+        }
+    }
+    @objc
+    func btnsTapped(_ sender: UIButton) {
+        guard let input = sender.titleLabel?.text else { return }
+        if expression.prefix(1) == "0" {
+            expression.removeFirst()
+        }
+        switch input {
+        case "AC":
+            expression = "0"
+        case "=":
+            let calcExp = expression.replacingOccurrences(of: Buttons.multiplMark, with: "*").replacingOccurrences(of: Buttons.divisionMark, with: "/")
+            guard let result = calculate(expression: calcExp) else { return }
+            expression = String(result)
+        case _ where Buttons.operators.contains(input):
+            if Buttons.operators.contains(String(expression.last ?? " ")) {
+                expression.removeLast()
+            }
+            expression.append(input)
+        default:
+            expression.append(input)
+        }
+    }
+    func calculate(expression: String) -> Int? {
+        let expression = NSExpression(format: expression)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+            return result
+        } else {
+            return nil
+        }
+    }
 }
-
+// MARK: Model
+struct Buttons {
+    private init() {}
+    static let multiplMark = "×"
+    static let divisionMark = "÷"
+    static let texts = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", multiplMark, "AC", "0", "=", divisionMark]
+    static let orrangeBtns = ["+", "-", divisionMark, multiplMark, "AC", "="]
+    static let operators = ["+", "-", divisionMark, multiplMark]
+}
